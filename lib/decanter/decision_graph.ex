b86fb29506fa177799efd769a5dbaaeed65c25bc
Chris Spencer <chris.ag.spencer@googlemail.com>
2014-12-14 22:34:40 +0000

    Clean up compile strategy

--- lib/decanter/decision_graph.ex
+++ lib/decanter/decision_graph.ex
@@ -25,12 +25,10 @@ defmodule Decanter.DecisionGraph do
 
     counts = visit_nodes(env.module, entry_name, nodes, %{})
 
-    {rewritten_entry_name, r} = compile_decision(env.module, entry_name, {counts, nodes}, %{})
-    # IO.inspect {"done", entry_name}
-    fn_bodies = for {_, body} <- r do
-      # IO.puts Macro.to_string(body)
-      body
-    end
+    {rewritten_entry_name, trees} = build_trees(env.module, entry_name, {counts, nodes}, %{})
+    fn_bodies = compile_trees(trees)
+
+    # IO.puts Macro.to_string(fn_bodies)
 
     if entry_name != rewritten_entry_name do
       fn_bodies = [quote do
@@ -78,125 +76,142 @@ defmodule Decanter.DecisionGraph do
     end
   end
 
-  defp compile_decision(module, name, {counts, nodes}=cnodes, bodies) do
-    if bodies[name] do
-      {name, bodies}
+  defp build_trees(module, name, {counts, nodes}=cnodes, trees) do
+    if trees[name] do
+      {name, trees}
     else
       case nodes[name] do
         {:branch, test, consequent, alternate} ->
           case Module.get_attribute(module, :decisions)[test] do
-            true -> compile_decision(module, consequent, cnodes, bodies)
-            false -> compile_decision(module, alternate, cnodes, bodies)
+            true -> build_trees(module, consequent, cnodes, trees)
+            false -> build_trees(module, alternate, cnodes, trees)
             body ->
-              {consequent, bodies} = compile_decision(module, consequent, cnodes, bodies)
-              {alternate, bodies} = compile_decision(module, alternate, cnodes, bodies)
+              {consequent, trees} = build_trees(module, consequent, cnodes, trees)
+              {alternate, trees} = build_trees(module, alternate, cnodes, trees)
               same_path = consequent == alternate
 
               if counts[consequent] == 1 || (same_path && counts[consequent] == 2) do
-                {:defp, _, [{:do_decide, _, _},[{:do, x}]]} = bodies[consequent]
-                consequent_body = x
-                bodies = Map.delete(bodies, consequent)
+                {:tree, consequent_body} = trees[consequent]
+                trees = Map.delete(trees, consequent)
               else
-                consequent_body = quote do
-                  do_decide(unquote(consequent), var!(conn))
-                end
+                consequent_body = {:call, consequent}
               end
 
               if counts[alternate] == 1 do
-                {:defp, _, [{:do_decide, _, _},[{:do, x}]]} = bodies[alternate]
-                alternate_body = x
-                bodies = Map.delete(bodies, alternate)
+                {:tree, alternate_body} = trees[alternate]
+                trees = Map.delete(trees, alternate)
               else
-                alternate_body = quote do
-                  do_decide(unquote(alternate), var!(conn))
-                end
+                alternate_body = {:call, alternate}
               end
 
+              # TODO: can this detection be cleaned up?
+              # has_header is internal helper, check_setting is from tests
               strategy = case body do
                 {:==, _, _} -> :if
                 {:in, _, _} -> :if
                 {:has_header, _, _} -> :if
+                {:check_setting, _, _} -> :if
                 _ -> :case
               end
 
               body = case {same_path, strategy} do
                 {true, :if} ->
-                  quote location: :keep do
-                    defp do_decide(unquote(name), var!(conn)) do
-                      unquote(consequent_body)
-                    end
-                  end
+                  consequent_body
                 {true, :case} ->
-                  quote location: :keep do
-                    defp do_decide(unquote(name), var!(conn)) do
-                      {_, var!(conn)} = handle_decision(var!(conn), unquote(body))
-                      unquote(consequent_body)
-                    end
-                  end
+                  {:block, body, consequent_body}
                 {false, :if} ->
-                  quote location: :keep do
-                    defp do_decide(unquote(name), var!(conn)) do
-                      if unquote(body) do
-                        unquote(consequent_body)
-                      else
-                        unquote(alternate_body)
-                      end
-                    end
-                  end
+                  {:if, body, consequent_body, alternate_body}
                 {false, :case} ->
-                  quote location: :keep do
-                    defp do_decide(unquote(name), var!(conn)) do
-                      case handle_decision(var!(conn), unquote(body)) do
-                        {true, var!(conn)} -> unquote(consequent_body)
-                        {false, var!(conn)} -> unquote(alternate_body)
-                      end
-                    end
-                  end
+                  {:case_dec, body, consequent_body, alternate_body}
               end
 
-              {name, Map.put(bodies, name, body)}
-            end
+              {name, Map.put(trees, name, {:tree, body})}
+          end
         {:handler, status, content} ->
           handlers = Module.get_attribute(module, :handlers)
 
-          if Map.has_key?(handlers, name) do
-            handles = for {match,body} <- Enum.reverse(handlers[name]) do
-              quote do
-                unquote(match) -> unquote(body)
-              end
-            end
-
-            body = quote location: :keep do
-              defp do_decide(unquote(name), var!(conn)) do
-                content = case var!(conn) do
-                  unquote(handles |> List.flatten)
-                end
-
-                Plug.Conn.resp(var!(conn), unquote(status), content)
-              end
-            end
+          body = if Map.has_key?(handlers, name) do
+            {:handler, status, :case, Enum.reverse(handlers[name])}
           else
-            body = quote location: :keep do
-              defp do_decide(unquote(name), var!(conn)) do
-                Plug.Conn.resp(var!(conn), unquote(status), unquote(content))
-              end
-            end
+            {:handler, status, :string, content}
           end
 
-          {name, Map.put(bodies, name, body)}
+          {name, Map.put(trees, name, {:tree, body})}
         {:action, next} ->
-          {next, bodies} = compile_decision(module, next, cnodes, bodies)
-
-          body = quote location: :keep do
-            defp do_decide(unquote(name), var!(conn)) do
-              do_decide(unquote(next), unquote(name)(var!(conn)))
-            end
-          end
-
-          {name, Map.put(bodies, name, body)}
+          {next, trees} = build_trees(module, next, cnodes, trees)
+          {name, Map.put(trees, name, {:tree, {:action, name, next}})}
       end
     end
   end
+
+  defp compile_trees(trees) do
+    for tree <- trees do
+      compile_tree(tree)
+    end
+  end
+
+  defp compile_tree({name, {:tree, body}}) do
+    quote location: :keep do
+      defp do_decide(unquote(name), var!(conn)) do
+        unquote(compile_node body)
+      end
+    end
+  end
+
+  defp compile_node({:call, name}) do
+    quote do
+      do_decide(unquote(name), var!(conn))
+    end
+  end
+  defp compile_node({:block, body, consequent}) do
+    quote location: :keep do
+      {_, var!(conn)} = handle_decision(var!(conn), unquote(body))
+      unquote(compile_node consequent)
+    end
+  end
+  defp compile_node({:if, body, consequent, alternate}) do
+    quote location: :keep do
+      if unquote(body) do
+        unquote(compile_node consequent)
+      else
+        unquote(compile_node alternate)
+      end
+    end
+  end
+  defp compile_node({:case_dec, body, consequent, alternate}) do
+    quote location: :keep do
+      case handle_decision(var!(conn), unquote(body)) do
+        {true, var!(conn)} -> unquote(compile_node consequent)
+        {false, var!(conn)} -> unquote(compile_node alternate)
+      end
+    end
+  end
+  defp compile_node({:handler, status, :case, handlers}) do
+    handles = for {match,body} <- handlers do
+      quote do
+        unquote(match) -> unquote(body)
+      end
+    end
+
+    quote do
+      content = case var!(conn) do
+        unquote(handles |> List.flatten)
+      end
+
+      Plug.Conn.resp(var!(conn), unquote(status), content)
+    end
+  end
+  defp compile_node({:handler, status, :string, content}) do
+    quote do
+      Plug.Conn.resp(var!(conn), unquote(status), unquote(content))
+    end
+  end
+  defp compile_node({:action, name, next}) do
+    quote do
+      do_decide(unquote(next), unquote(name)(var!(conn)))
+    end
+  end
+
 
   defmacro branch(name, test, consequent, alternate) do
     quote do
