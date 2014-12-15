90f463459dd599cadf6039d188287d93fd164858
Chris Spencer <chris.ag.spencer@googlemail.com>
2014-12-15 22:10:45 +0000

    Add mix task to generate the dotfile

---
+++ lib/mix/tasks/decanter.dotfile.ex
@@ -0,0 +1,19 @@
+defmodule Mix.Tasks.Decanter.Dotfile.R do
+  use Decanter
+
+  plug :serve
+
+  def gen_dotfile do
+    generate_dot_file(@nodes)
+  end
+end
+
+defmodule Mix.Tasks.Decanter.Dotfile do
+  use Mix.Task
+
+  @shortdoc "Create the dotfile for the default decision tree"
+
+  def run(args) do
+    IO.puts Mix.Tasks.Decanter.Dotfile.R.gen_dotfile
+  end
+end
